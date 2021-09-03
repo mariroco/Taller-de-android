efe98c5a6d275732668ccbc8ee749cba427b7b86
Mariela Rocha <marielarc101@hotmail.com>
2021-09-02 20:05:10 -0500

    Views added

--- PracticaActivities/app/src/main/java/com/mariroco/practicaactivities/MainActivity.kt
+++ PracticaActivities/app/src/main/java/com/mariroco/practicaactivities/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initVars()
+        initViews()
+        setEvents()
     }
+
+    //Initializes all var values
+    fun initVars(){}
+
+    //Initializes activity_main.xml
+    fun initViews(){}
+
+    //Sets view elements' events (onclick...)
+    fun setEvents(){}
 }
---
+++ PracticaActivities/app/src/main/java/com/mariroco/practicaactivities/imageDetail.kt
@@ -0,0 +1,11 @@
+package com.mariroco.practicaactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class imageDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_detail)
+    }
+}
