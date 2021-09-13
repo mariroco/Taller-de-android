d637b53e47ccb83680fcac9736a47af2899849f4
Mariela Rocha <marielarc101@hotmail.com>
2021-09-13 15:15:50 -0500

    Fragments added, now accesible

--- PracticaFragments/app/src/main/java/com/mariroco/practicafragments/DetailFragment.kt
+++ PracticaFragments/app/src/main/java/com/mariroco/practicafragments/DetailFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,18 +19,41 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class DetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var imgMain: ImageView
+    private lateinit var txtDetail: TextView
+    private lateinit var imgFav: ImageView
+
+    //private var param1: String? = null
+    //private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            //param1 = it.getString(ARG_PARAM1)
+            //param2 = it.getString(ARG_PARAM2)
         }
     }
 
+
+    fun initView(){
+        imgMain = requireView().findViewById(R.id.img_main)
+        txtDetail = requireView().findViewById(R.id.txt_detail)
+        imgFav = requireView().findViewById(R.id.img_fav)
+
+        imgMain.setOnClickListener {
+           changeFrag()
+        }
+    }
+
+    fun changeFrag(){
+        val frag=ImageFragment.newInstance()
+        (activity as MainActivity).replaceFragment(frag,ImageFragment.TAG)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as MainActivity?)?.changeVisibility()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,23 +62,15 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+        val TAG = DetailFragment::class.java.simpleName
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = DetailFragment()
     }
 }
--- PracticaFragments/app/src/main/java/com/mariroco/practicafragments/MainActivity.kt
+++ PracticaFragments/app/src/main/java/com/mariroco/practicafragments/MainActivity.kt
@@ -2,8 +2,15 @@ package com.mariroco.practicafragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +18,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var imgvPrevious: ImageView
     private lateinit var imgvNext: ImageView
     private lateinit var btnInfo: Button
+    private lateinit var containerMain : ConstraintLayout
+    private lateinit var containerFrag : FrameLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +33,29 @@ class MainActivity : AppCompatActivity() {
         imgvPrevious = findViewById(R.id.imgv_previous)
         imgvNext = findViewById(R.id.imgv_next)
         btnInfo = findViewById(R.id.btn_Info)
+        containerMain = findViewById(R.id.container_main)
+        containerFrag = findViewById(R.id.container)
+
+        btnInfo.setOnClickListener {
+            changeVisibility()
+            supportFragmentManager.beginTransaction().add(R.id.container, DetailFragment()).addToBackStack(null).commit()
+
+        }
+    }
+
+    public fun changeVisibility(){
+        if (containerMain.visibility == VISIBLE){
+            containerMain.visibility = GONE
+            containerFrag.visibility = VISIBLE
+        }else{
+            containerMain.visibility = VISIBLE
+            containerFrag.visibility = GONE
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment, tag).addToBackStack("").commit()
     }
 
 }
