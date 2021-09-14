222add6315fc9b518afffe9d9c8ab6ad4a5f8201
Mariela Rocha <marielarc101@hotmail.com>
2021-09-13 23:22:26 -0500

    last fragment implemented

--- PracticaFragments/app/src/main/java/com/mariroco/practicafragments/DetailFragment.kt
+++ PracticaFragments/app/src/main/java/com/mariroco/practicafragments/DetailFragment.kt
@@ -26,8 +26,6 @@ class DetailFragment : Fragment() {
     private lateinit var imageList: List<Image>
     var counter : Int=0
 
-    //private var param1: String? = null
-    //private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- PracticaFragments/app/src/main/java/com/mariroco/practicafragments/ImageFragment.kt
+++ PracticaFragments/app/src/main/java/com/mariroco/practicafragments/ImageFragment.kt
@@ -5,27 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ImageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ImageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var imageList: List<Image>
+    var counter : Int=0
+    private lateinit var imgMain: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -37,8 +31,16 @@ class ImageFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_image, container, false)
     }
 
-    fun initView(){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
 
+    fun initView(){
+        counter = (activity as MainActivity).counter
+        imageList = (activity as MainActivity).imageList
+        imgMain=requireView().findViewById(R.id.img_main)
+        imgMain.setImageResource(imageList[counter].image)
     }
 
     companion object {
