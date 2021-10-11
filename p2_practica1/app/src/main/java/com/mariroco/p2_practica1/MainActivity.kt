180615a89157f4d16d5698524944fe80ffd8b35f
Mariela Rocha <marielarc101@hotmail.com>
2021-10-11 00:19:14 -0500

    creación

---
+++ p2_practica1/app/src/main/java/com/mariroco/p2_practica1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mariroco.p2_practica1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
