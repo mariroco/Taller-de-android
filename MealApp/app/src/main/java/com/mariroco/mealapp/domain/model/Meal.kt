315250c8f8e116babebd7fee718d186b8795e77a
Mariela Rocha <marielarc101@hotmail.com>
2021-10-21 21:58:39 -0500

    row and grid layout added

---
+++ MealApp/app/src/main/java/com/mariroco/mealapp/domain/model/Meal.kt
@@ -0,0 +1,24 @@
+package com.mariroco.mealapp.domain.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
+import org.w3c.dom.ProcessingInstruction
+
+@Parcelize
+//@Entity
+//@JsonClass(generateAdapter = true)
+class Meal(
+    @PrimaryKey(autoGenerate = false)
+    val idMeal : Int= 0,
+    @Json(name = "strMeal") val name: String = "",
+    @Json(name="strCategory") val category: String="",
+    @Json(name="strMealThumb") val urlThumb:  String? = "",
+    @Json(name="strArea") val area: String = "",
+    @Json(name = "strInstructions") val instructions: String?=""
+
+):Parcelable {
+}
