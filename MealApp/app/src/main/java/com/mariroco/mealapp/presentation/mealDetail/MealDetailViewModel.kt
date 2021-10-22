6f19256c61ee2c4c156ca06447faa0e1a17e8fad
Mariela Rocha <marielarc101@hotmail.com>
2021-10-22 14:20:40 -0500

    fragments added
    MealDetaill fragment added
    minor changes in the rest of the app

---
+++ MealApp/app/src/main/java/com/mariroco/mealapp/presentation/mealDetail/MealDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.mariroco.mealapp.presentation.mealDetail
+
+import androidx.lifecycle.ViewModel
+
+class MealDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ MealApp/app/src/main/java/com/mariroco/mealapp/presentation/mealDetail/mealDetailFragment.kt
@@ -0,0 +1,18 @@
+package com.mariroco.mealapp.presentation.mealDetail
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mariroco.mealapp.R
+import com.mariroco.mealapp.core.presentation.BaseFragment
+
+class mealDetailFragment : BaseFragment(R.layout.meal_detail_fragment) {
+    override fun setBinding(view: View) {
+        TODO("Not yet implemented")
+    }
+
+
+}
