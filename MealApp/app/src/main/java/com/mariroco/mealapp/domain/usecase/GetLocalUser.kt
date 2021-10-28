9f7468394239d69b25af53907f2e4a3baa7bd599
Mariela Rocha <marielarc101@hotmail.com>
2021-10-27 22:14:17 -0500

    Account hyperlink now works

--- MealApp/app/src/main/java/com/mariroco/mealapp/domain/usecase/GetLocalUser.kt
+++ MealApp/app/src/main/java/com/mariroco/mealapp/domain/usecase/GetLocalUser.kt
@@ -1,4 +1,21 @@
 package com.mariroco.mealapp.domain.usecase
 
-class GetLocalUser {
+import com.mariroco.mealapp.core.exception.Failure
+import com.mariroco.mealapp.core.functional.Either
+import com.mariroco.mealapp.core.interactor.UseCase
+import com.mariroco.mealapp.core.platform.AuthManager
+import com.mariroco.mealapp.domain.model.User
+import javax.inject.Inject
+
+class GetLocalUser @Inject constructor(private val authManager: AuthManager) :
+    UseCase<User, UseCase.None>() {
+
+    override suspend fun run(params: None): Either<Failure, User> {
+        val result = authManager.user
+
+        return result?.let {
+            Either.Right(it)
+        } ?: Either.Left(Failure.Unauthorized)
+    }
+
 }
--- MealApp/app/src/main/java/com/mariroco/mealapp/presentation/profile/ProfileFragment.kt
+++ MealApp/app/src/main/java/com/mariroco/mealapp/presentation/profile/ProfileFragment.kt
@@ -6,12 +6,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.mariroco.mealapp.R
 import com.mariroco.mealapp.core.presentation.BaseFragment
+import com.mariroco.mealapp.databinding.ProfileFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 class ProfileFragment : BaseFragment(R.layout.profile_fragment) {
+    private lateinit var binding: ProfileFragmentBinding
+
     override fun setBinding(view: View) {
+        binding = ProfileFragmentBinding.bind(view)
+        binding.lifecycleOwner = this
+
+        binding.txtNewAccount.setOnClickListener(){
+            binding.loginContainer.isVisible = false
+            binding.newAccountContainer.isVisible = true
+        }
+
+        binding.txtLoginExisting.setOnClickListener(){
+            binding.loginContainer.isVisible = true
+            binding.newAccountContainer.isVisible = false
+        }
+
     }
 
 }
